756180ac42a3409991099e362535dab1ba673773
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2022-11-21 12:38:17 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# p-unite-info-site
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-qmhhfv)
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <div className="bg-gray-400 px-2 py-2"></div>
+    </>
+  );
+};
+
+export default App;
